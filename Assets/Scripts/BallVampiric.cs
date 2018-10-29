c7077f2b336478c7a2ad033ee70cbb430c307fbd
dyzalonius <dyzalonius@gmail.com>
2018-10-29 21:02:14 +0100

    Added vampire effect to vampire ball.

--- Assets/Scripts/BallVampiric.cs
+++ Assets/Scripts/BallVampiric.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 
 public class BallVampiric : Ball {
 
+    [SerializeField] GameObject vampirePrefab;
+    PlayerManager playerManager;
+
     protected override void Start() {
         base.Start();
     }
 
     public override void OnBounce(PlayerManager playerManager) {
-        LifeSteal(playerManager);
+        this.playerManager = playerManager;
+        SpawnVampire();
 
         base.OnBounce(playerManager);
     }
 
-    void LifeSteal(PlayerManager playerManager) {
+    void SpawnVampire() {
+        GameObject vampire = Instantiate(vampirePrefab, playerManager.transform.position, Quaternion.Euler(0, 0, 0));
+        vampire.GetComponent<VampireManager>().Initialize(playerManager);
+        Invoke("LifeSteal", 0.4f);
+    }
+
+    void LifeSteal() {
         ScoreboardManager scoreboardManager = playerManager.gameManager.scoreboardManager;
 
         if (playerManager.teamID == 1) {
---
+++ Assets/Scripts/VampireManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VampireManager : MonoBehaviour {
+
+    [SerializeField] float speedMax;
+    GameObject player1, player2;
+    Text panel1, panel2;
+    Vector3 targetPos;
+    bool travelling;
+    float speed;
+
+	// Use this for initialization
+	void Start () {
+        travelling = true;
+        speed = speedMax / 20;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (travelling) {
+            Move();
+        } else {
+            FadeOut();
+        }
+	}
+
+    void Move() {
+        Vector3 direction = targetPos - transform.position;
+
+        if (direction.magnitude < speed) {
+            travelling = false;
+            transform.position = targetPos;
+        }
+        else {
+            direction.Normalize();
+            speed += speedMax / 100;
+            transform.position += direction * speed;
+        }
+    }
+
+    void FadeOut() {
+        GetComponent<SpriteRenderer>().color -= new Color(0, 0, 0, 0.1f);
+    }
+
+    public void Initialize(PlayerManager playerManager) {
+        player1 = playerManager.gameManager.player1;
+        player2 = playerManager.gameManager.player2;
+        panel1 = playerManager.gameManager.scoreboardManager.hits1Text;
+        panel2 = playerManager.gameManager.scoreboardManager.hits2Text;
+
+        switch (playerManager.teamID) {
+            case 1:
+                transform.position = player1.transform.position;
+                targetPos = panel1.transform.position;
+                break;
+            case 2:
+                transform.position = player2.transform.position;
+                targetPos = panel2.transform.position;
+                break;
+        }
+    }
+}
